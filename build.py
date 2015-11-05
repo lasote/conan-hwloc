fa68767f1431e11dafa29a15f67bf67aea5be176
Luis Martinez <lasote@gmail.com>
2015-11-05 10:46:27 +0100

    Windows working

--- build.py
+++ build.py
@@ -14,30 +14,25 @@ if __name__ == "__main__":
 
 
     if platform.system() == "Windows":
+        print("It will fail first time because of adjustement of visual project. Open 'sln' project in hwloc\1.11.1\lasote\stable\source\hwloc-1.11.1\contrib/windows, update the solution and add x86 as configuration available ")
+        raw_input("Press Enter to continue...")
+        
         compiler = '-s compiler="Visual Studio" -s compiler.version=12 '
         # Static x86
-        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
-        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
-        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
-        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o hwloc:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o hwloc:shared=False')
 
         # Static x86_64
-        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
-        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
-        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
-        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o hwloc:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o hwloc:shared=False')
 
         # Shared x86
-        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
-        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
-        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
-        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o hwloc:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o hwloc:shared=True')
 
         # Shared x86_64
-        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
-        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
-        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
-        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o hwloc:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o hwloc:shared=True')
 
     else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
         if not os.getenv("TRAVIS", False):  
